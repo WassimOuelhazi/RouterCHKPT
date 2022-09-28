745aa7c042f1c692ed505f57f634ab274d34d387
WassimOuelhazi <ouelhaziw@gmail.com>
2022-09-28 07:51:29 +0100

    adding iframe trailer

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,39 +1,40 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import {Link} from 'react-router-dom';
 
 const MovieDetails = ({ Movies }) => {
   const { id } = useParams();
 
-  const movie = Movies.find((m) => m._id ===id ) ;
-
-
+  const movie = Movies.find((m) => m._id === id);
 
   return (
-    <div Style="margin: 10% 35%;width:600px;height:600px">
+    <div Style="margin: 0% 35%;width:600px;height:600px">
       <div className="cardDetails">
-        
         <div className="title">{movie.title}</div>
 
-          <img className="img1" alt="movie" src={movie.imgUrl} />
-          <div className="text">{movie.description}</div>
-          <a href="/">
-            <div className="catagory">
-              {movie.category} <i className="fas fa-film"></i>
-            </div>
-          </a>
-          <a href={movie.trailerUrl}>
-            <div className="views">
-              Trailer <i className="far fa-eye"></i>{" "}
-            </div>
-          </a>
-
-          <Link className="link" to={movie.trailerUrl}>
-        </Link>
-
-         
-          
-        
+        <img className="img1" alt="movie" src={movie.imgUrl} />
+        <div className="text">{movie.description}</div>
+        <a href="/">
+          <div className="catagory">
+            {movie.category} <i className="fas fa-film"></i>
+          </div>
+        </a>
+        <a href="/">
+          <div className="views">
+          {movie.date} <i className="far fa-eye"></i>{" "}
+          </div>
+        </a>
+      </div>
+      <div>
+      <h2 Style="color:white">Trailer</h2> 
+        <iframe
+          width="560"
+          height="315"
+          src={movie.trailerUrl}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   );
